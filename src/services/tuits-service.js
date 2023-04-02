e1b652506f111f1f5ac5aeb7fa2e72f932a145ec
Sai Dheeraj Reddy Gadwala <drgad24@gmail.com>
2023-04-01 23:56:55 -0400

    dislikes

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-// const TUITS_API = 'http://localhost:4000/api/tuits';
+//const API_BASE = 'http://localhost:4000/api';
 const API_BASE = process.env.REACT_APP_API_BASE;
 const TUITS_API = `${API_BASE}/tuits`;
 
@@ -25,5 +25,4 @@ export const findTuits = async () => {
       .put(`${TUITS_API}/${tuit._id}`, tuit);
     return tuit;
   }
-  
   
--- src/tuiter/tuits/tuit-stats.js
+++ src/tuiter/tuits/tuit-stats.js
@@ -15,19 +15,34 @@ const TuitStat = (post) => {
             newLikes += 1;
         dispatch(updateTuitThunk({...post, liked: !post.liked, likes: newLikes}));
     }   
+    const toggleDislikeHandler = (post) => {
+        let newDislikes = post.dislikes;
+        if (post.disliked)
+            newDislikes -= 1;
+        else
+            newDislikes += 1;
+        dispatch(updateTuitThunk({...post, disliked: !post.disliked, dislikes: newDislikes}));
+    }   
 
     return(
         <div className="wd-icons">
-            <a href="#"><i className="fa-regular fa-comment"></i><span>{post.replies}</span></a>
-            <a href="#"><i className='fa fa-retweet'></i><span>{post.retuits}</span></a>
-            <a href="#" onClick={()=>toggleLikeHandler(post)}>
+            <a><i className="fa-regular fa-comment"></i><span>{post.replies}</span></a>
+            <a><i className='fa fa-retweet'></i><span>{post.retuits}</span></a>
+            <a onClick={()=>toggleLikeHandler(post)}>
             {
                 post.liked ?
                 <i className="bi bi-suit-heart-fill wd-red-heart"></i>
                 :<i className="bi bi-suit-heart"></i>
             }
             <span>{post.likes}</span></a>
-            <a href="#"><i className='fa fa-share'></i></a>
+            <a onClick={()=>toggleDislikeHandler(post)}>
+            {
+                post.disliked ?
+                <i class="bi bi-hand-thumbs-down-fill wd-black-dislike"></i>
+                :<i className="bi bi-hand-thumbs-down"></i>
+            }
+            <span>{post.dislikes}</span></a>
+            <a><i className='fa fa-share'></i></a>
             <div></div>
         </div>
         
